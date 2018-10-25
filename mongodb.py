1fe93ca642317ecb1ca9555058e4d26a32c24aa6
guaik <luting.gu@gmail.com>
2018-10-25 22:10:05 +0800

    update mongodb

--- mongodb.py
+++ mongodb.py
@@ -4,6 +4,9 @@ import getpass
 from sys import argv
 
 install = """
+yum -y install epel-release
+yum -y install wget
+yum -y update
 echo "[mongodb-org-3.2]
 name=MongoDB Repository
 baseurl=https://repo.mongodb.org/yum/redhat/$releasever/mongodb-org/3.2/x86_64/
